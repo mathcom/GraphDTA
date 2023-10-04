267f56b11ef18bdbb5bea8048c2577f8d1a3091f
mathcom <jonghwanc7757@gmail.com>
2023-10-04 16:55:01 +0900

    Refactor: generate a conda env file and clean the scripts

--- create_data.py
+++ create_data.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
 import os
-import json,pickle
+import json, pickle
 from collections import OrderedDict
 from rdkit import Chem
 from rdkit.Chem import MolFromSmiles
 import networkx as nx
 from utils import *
+
 
 def atom_features(atom):
     return np.array(one_of_k_encoding_unk(atom.GetSymbol(),['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na','Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb','Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H','Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr','Cr', 'Pt', 'Hg', 'Pb', 'Unknown']) +
@@ -15,16 +16,19 @@ def atom_features(atom):
                     one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
                     [atom.GetIsAromatic()])
 
+
 def one_of_k_encoding(x, allowable_set):
     if x not in allowable_set:
         raise Exception("input {0} not in allowable set{1}:".format(x, allowable_set))
     return list(map(lambda s: x == s, allowable_set))
+
 
 def one_of_k_encoding_unk(x, allowable_set):
     """Maps inputs not in the allowable set to the last element."""
     if x not in allowable_set:
         x = allowable_set[-1]
     return list(map(lambda s: x == s, allowable_set))
+
 
 def smile_to_graph(smile):
     mol = Chem.MolFromSmiles(smile)
@@ -46,94 +50,128 @@ def smile_to_graph(smile):
         
     return c_size, features, edge_index
 
-def seq_cat(prot):
+
+def seq_cat(prot, max_seq_len, seq_dict):
     x = np.zeros(max_seq_len)
     for i, ch in enumerate(prot[:max_seq_len]): 
         x[i] = seq_dict[ch]
     return x  
 
 
-# from DeepDTA data
-all_prots = []
-datasets = ['kiba','davis']
-for dataset in datasets:
-    print('convert data from DeepDTA for ', dataset)
-    fpath = 'data/' + dataset + '/'
-    train_fold = json.load(open(fpath + "folds/train_fold_setting1.txt"))
-    train_fold = [ee for e in train_fold for ee in e ]
-    valid_fold = json.load(open(fpath + "folds/test_fold_setting1.txt"))
-    ligands = json.load(open(fpath + "ligands_can.txt"), object_pairs_hook=OrderedDict)
-    proteins = json.load(open(fpath + "proteins.txt"), object_pairs_hook=OrderedDict)
-    affinity = pickle.load(open(fpath + "Y","rb"), encoding='latin1')
-    drugs = []
-    prots = []
-    for d in ligands.keys():
-        lg = Chem.MolToSmiles(Chem.MolFromSmiles(ligands[d]),isomericSmiles=True)
-        drugs.append(lg)
-    for t in proteins.keys():
-        prots.append(proteins[t])
-    if dataset == 'davis':
-        affinity = [-np.log10(y/1e9) for y in affinity]
-    affinity = np.asarray(affinity)
-    opts = ['train','test']
-    for opt in opts:
-        rows, cols = np.where(np.isnan(affinity)==False)  
-        if opt=='train':
-            rows,cols = rows[train_fold], cols[train_fold]
-        elif opt=='test':
-            rows,cols = rows[valid_fold], cols[valid_fold]
-        with open('data/' + dataset + '_' + opt + '.csv', 'w') as f:
-            f.write('compound_iso_smiles,target_sequence,affinity\n')
-            for pair_ind in range(len(rows)):
-                ls = []
-                ls += [ drugs[rows[pair_ind]]  ]
-                ls += [ prots[cols[pair_ind]]  ]
-                ls += [ affinity[rows[pair_ind],cols[pair_ind]]  ]
-                f.write(','.join(map(str,ls)) + '\n')       
-    print('\ndataset:', dataset)
-    print('train_fold:', len(train_fold))
-    print('test_fold:', len(valid_fold))
-    print('len(set(drugs)),len(set(prots)):', len(set(drugs)),len(set(prots)))
-    all_prots += list(set(prots))
+def main():
+    # from DeepDTA data
+    all_prots = []
+    DATASETS = ['kiba', 'davis']
     
+    for dataset in DATASETS:
+        print(f'[INFO] Convert data from DeepDTA for {dataset}')
+        
+        ## Directory & Filepathes
+        fpath = os.path.join('data', dataset)
+        train_fold = json.load(open(os.path.join(fpath, 'folds', 'train_fold_setting1.txt')))
+        train_fold = [ee for e in train_fold for ee in e ]
+        valid_fold = json.load(open(os.path.join(fpath, 'folds', 'test_fold_setting1.txt')))
+        ligands = json.load(open(os.path.join(fpath, "ligands_can.txt")), object_pairs_hook=OrderedDict)
+        proteins = json.load(open(os.path.join(fpath, "proteins.txt")), object_pairs_hook=OrderedDict)
+        affinity = pickle.load(open(os.path.join(fpath, "Y"),"rb"), encoding='latin1')
+        
+        ## Data loading
+        print(f'[DEBUG] Data Loading')
+        drugs = []
+        prots = []
+        for d in ligands.keys():
+            lg = Chem.MolToSmiles(Chem.MolFromSmiles(ligands[d]), isomericSmiles=True)
+            drugs.append(lg)
+            
+        for t in proteins.keys():
+            prots.append(proteins[t])
+        
+        ## Normalization
+        if dataset == 'davis':
+            affinity = [-np.log10(y * 1e-9) for y in affinity]
+        
+        affinity = np.asarray(affinity)
+        
+        ## Fold
+        print(f'[DEBUG] Partioning into training and test folds')
+        opts = ['train','test']
+        for opt in opts:
+            rows, cols = np.where(np.isnan(affinity)==False)  
+            
+            if opt =='train':
+                rows,cols = rows[train_fold], cols[train_fold]
+            elif opt =='test':
+                rows,cols = rows[valid_fold], cols[valid_fold]
+            
+            with open(os.path.join('data', f'{dataset}_{opt}.csv'), 'w') as f:
+                f.write('compound_iso_smiles,target_sequence,affinity\n')
+                for pair_ind in range(len(rows)):
+                    ls = []
+                    ls += [ drugs[rows[pair_ind]]  ]
+                    ls += [ prots[cols[pair_ind]]  ]
+                    ls += [ affinity[rows[pair_ind],cols[pair_ind]]  ]
+                    f.write(','.join(map(str,ls)) + '\n')       
+        
+        print()
+        print(f'[INFO] dataset: {dataset}')
+        print(f'[INFO] train_fold: {len(train_fold)}')
+        print(f'[INFO] test_fold: {len(valid_fold)}')
+        print(f'[INFO] len(set(drugs)), len(set(prots)): {len(set(drugs))}, {len(set(prots))}')
+        
+        all_prots += list(set(prots))
+        
+        
+    seq_voc = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+    seq_dict = {v:(i+1) for i,v in enumerate(seq_voc)}
+    max_seq_len = 1000
+
+    compound_iso_smiles = []
+    for dt_name in DATASETS:
+        opts = ['train', 'test']
+        for opt in opts:
+            df = pd.read_csv(os.path.join('data', f'{dt_name}_{opt}.csv'))
+            compound_iso_smiles += list( df['compound_iso_smiles'] )
+            
+    compound_iso_smiles = set(compound_iso_smiles)
     
-seq_voc = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
-seq_dict = {v:(i+1) for i,v in enumerate(seq_voc)}
-seq_dict_len = len(seq_dict)
-max_seq_len = 1000
+    smile_graph = {}
+    for smile in compound_iso_smiles:
+        g = smile_to_graph(smile)
+        smile_graph[smile] = g
 
-compound_iso_smiles = []
-for dt_name in ['kiba','davis']:
-    opts = ['train','test']
-    for opt in opts:
-        df = pd.read_csv('data/' + dt_name + '_' + opt + '.csv')
-        compound_iso_smiles += list( df['compound_iso_smiles'] )
-compound_iso_smiles = set(compound_iso_smiles)
-smile_graph = {}
-for smile in compound_iso_smiles:
-    g = smile_to_graph(smile)
-    smile_graph[smile] = g
 
-datasets = ['davis','kiba']
-# convert to PyTorch data format
-for dataset in datasets:
-    processed_data_file_train = 'data/processed/' + dataset + '_train.pt'
-    processed_data_file_test = 'data/processed/' + dataset + '_test.pt'
-    if ((not os.path.isfile(processed_data_file_train)) or (not os.path.isfile(processed_data_file_test))):
-        df = pd.read_csv('data/' + dataset + '_train.csv')
-        train_drugs, train_prots,  train_Y = list(df['compound_iso_smiles']),list(df['target_sequence']),list(df['affinity'])
-        XT = [seq_cat(t) for t in train_prots]
-        train_drugs, train_prots,  train_Y = np.asarray(train_drugs), np.asarray(XT), np.asarray(train_Y)
-        df = pd.read_csv('data/' + dataset + '_test.csv')
-        test_drugs, test_prots,  test_Y = list(df['compound_iso_smiles']),list(df['target_sequence']),list(df['affinity'])
-        XT = [seq_cat(t) for t in test_prots]
-        test_drugs, test_prots,  test_Y = np.asarray(test_drugs), np.asarray(XT), np.asarray(test_Y)
+    ## convert to PyTorch data format
+    for dataset in DATASETS:
+        processed_data_file_train = os.path.join('data', 'processed', f'{dataset}_train.pt')
+        processed_data_file_test = os.path.join('data', 'processed', f'{dataset}_test.pt')
+        
+        if ((not os.path.isfile(processed_data_file_train)) or (not os.path.isfile(processed_data_file_test))):
+            
+            ## training data
+            df = pd.read_csv(os.path.join('data', f'{dataset}_train.csv'))
+            train_Y     = df['affinity'].values
+            train_drugs = df['compound_iso_smiles'].values
+            train_prots = df['target_sequence'].values.tolist()
+            train_prots = np.asarray([seq_cat(t, max_seq_len, seq_dict) for t in train_prots])
+            
+            ## test data
+            df = pd.read_csv(os.path.join('data', f'{dataset}_test.csv'))
+            test_Y     = df['affinity'].values
+            test_drugs = df['compound_iso_smiles'].values
+            test_prots = df['target_sequence'].values.tolist()
+            test_prots = np.asarray([seq_cat(t, max_seq_len, seq_dict) for t in test_prots])
+            
+            ## make data PyTorch Geometric ready
+            print(f'[INFO] preparing {dataset}_train.pt in pytorch format!')
+            train_data = TestbedDataset(root='data', dataset=f'{dataset}_train', xd=train_drugs, xt=train_prots, y=train_Y, smile_graph=smile_graph)
+            
+            print(f'[INFO] preparing {dataset}_test.pt in pytorch format!')
+            test_data = TestbedDataset(root='data', dataset=f'{dataset}_test', xd=test_drugs, xt=test_prots, y=test_Y, smile_graph=smile_graph)
+            
+            print(f'{processed_data_file_train} and {processed_data_file_test} have been created')
+        else:
+            print(f'{processed_data_file_train} and {processed_data_file_test} are already created')        
 
-        # make data PyTorch Geometric ready
-        print('preparing ', dataset + '_train.pt in pytorch format!')
-        train_data = TestbedDataset(root='data', dataset=dataset+'_train', xd=train_drugs, xt=train_prots, y=train_Y,smile_graph=smile_graph)
-        print('preparing ', dataset + '_test.pt in pytorch format!')
-        test_data = TestbedDataset(root='data', dataset=dataset+'_test', xd=test_drugs, xt=test_prots, y=test_Y,smile_graph=smile_graph)
-        print(processed_data_file_train, ' and ', processed_data_file_test, ' have been created')        
-    else:
-        print(processed_data_file_train, ' and ', processed_data_file_test, ' are already created')        
+
+if __name__=='__main__':
+    main()
--- training.py
+++ training.py
@@ -10,17 +10,21 @@ from models.gcn import GCNNet
 from models.ginconv import GINConvNet
 from utils import *
 
-# training function at each epoch
+
+## training function at each epoch
 def train(model, device, train_loader, optimizer, epoch):
-    print('Training on {} samples...'.format(len(train_loader.dataset)))
+    print(f'Training on {len(train_loader.dataset)} samples...')
     model.train()
+    
     for batch_idx, data in enumerate(train_loader):
         data = data.to(device)
-        optimizer.zero_grad()
         output = model(data)
+        
+        optimizer.zero_grad()
         loss = loss_fn(output, data.y.view(-1, 1).float().to(device))
         loss.backward()
         optimizer.step()
+        
         if batch_idx % LOG_INTERVAL == 0:
             print('Train epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch,
                                                                            batch_idx * len(data.x),
@@ -30,73 +34,92 @@ def train(model, device, train_loader, optimizer, epoch):
 
 def predicting(model, device, loader):
     model.eval()
+    
     total_preds = torch.Tensor()
     total_labels = torch.Tensor()
-    print('Make prediction for {} samples...'.format(len(loader.dataset)))
+    
+    print(f'Make prediction for {len(loader.dataset)} samples...')
+    
     with torch.no_grad():
         for data in loader:
             data = data.to(device)
             output = model(data)
             total_preds = torch.cat((total_preds, output.cpu()), 0)
             total_labels = torch.cat((total_labels, data.y.view(-1, 1).cpu()), 0)
-    return total_labels.numpy().flatten(),total_preds.numpy().flatten()
+            
+    return total_labels.numpy().flatten(), total_preds.numpy().flatten()
 
 
-datasets = [['davis','kiba'][int(sys.argv[1])]] 
-modeling = [GINConvNet, GATNet, GAT_GCN, GCNNet][int(sys.argv[2])]
-model_st = modeling.__name__
+def main():
+    datasets = [['davis','kiba'][int(sys.argv[1])]] 
+    modeling = [GINConvNet, GATNet, GAT_GCN, GCNNet][int(sys.argv[2])]
+    model_st = modeling.__name__
 
-cuda_name = "cuda:0"
-if len(sys.argv)>3:
-    cuda_name = "cuda:" + str(int(sys.argv[3])) 
-print('cuda_name:', cuda_name)
+    cuda_name = "cuda:0"
+    if len(sys.argv)>3:
+        cuda_name = "cuda:" + str(int(sys.argv[3])) 
+    print(f'cuda_name: {cuda_name}')
 
-TRAIN_BATCH_SIZE = 512
-TEST_BATCH_SIZE = 512
-LR = 0.0005
-LOG_INTERVAL = 20
-NUM_EPOCHS = 1000
+    ## Global Variables
+    print(f'Learning rate: {LR}')
+    print(f'Epochs: {NUM_EPOCHS}')
 
-print('Learning rate: ', LR)
-print('Epochs: ', NUM_EPOCHS)
-
-# Main program: iterate over different datasets
-for dataset in datasets:
-    print('\nrunning on ', model_st + '_' + dataset )
-    processed_data_file_train = 'data/processed/' + dataset + '_train.pt'
-    processed_data_file_test = 'data/processed/' + dataset + '_test.pt'
-    if ((not os.path.isfile(processed_data_file_train)) or (not os.path.isfile(processed_data_file_test))):
-        print('please run create_data.py to prepare data in pytorch format!')
-    else:
-        train_data = TestbedDataset(root='data', dataset=dataset+'_train')
-        test_data = TestbedDataset(root='data', dataset=dataset+'_test')
+    ## Main program: iterate over different datasets
+    for dataset in datasets:
+        print()
+        print(f'running on {model_st}_{dataset}')
         
-        # make data PyTorch mini-batch processing ready
-        train_loader = DataLoader(train_data, batch_size=TRAIN_BATCH_SIZE, shuffle=True)
-        test_loader = DataLoader(test_data, batch_size=TEST_BATCH_SIZE, shuffle=False)
+        processed_data_file_train = os.path.join('data', 'processed', f'{dataset}_train.pt')
+        processed_data_file_test = os.path.join('data', 'processed', f'{dataset}_test.pt')
+        
+        if ((not os.path.isfile(processed_data_file_train)) or (not os.path.isfile(processed_data_file_test))):
+            print('please run create_data.py to prepare data in pytorch format!')
+        else:
+            train_data = TestbedDataset(root='data', dataset=f'{dataset}_train')
+            test_data = TestbedDataset(root='data', dataset=f'{dataset}_test')
+            
+            ## make data PyTorch mini-batch processing ready
+            train_loader = DataLoader(train_data, batch_size=TRAIN_BATCH_SIZE, shuffle=True)
+            test_loader = DataLoader(test_data, batch_size=TEST_BATCH_SIZE, shuffle=False)
 
-        # training the model
-        device = torch.device(cuda_name if torch.cuda.is_available() else "cpu")
-        model = modeling().to(device)
-        loss_fn = nn.MSELoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=LR)
-        best_mse = 1000
-        best_ci = 0
-        best_epoch = -1
-        model_file_name = 'model_' + model_st + '_' + dataset +  '.model'
-        result_file_name = 'result_' + model_st + '_' + dataset +  '.csv'
-        for epoch in range(NUM_EPOCHS):
-            train(model, device, train_loader, optimizer, epoch+1)
-            G,P = predicting(model, device, test_loader)
-            ret = [rmse(G,P),mse(G,P),pearson(G,P),spearman(G,P),ci(G,P)]
-            if ret[1]<best_mse:
-                torch.save(model.state_dict(), model_file_name)
-                with open(result_file_name,'w') as f:
-                    f.write(','.join(map(str,ret)))
-                best_epoch = epoch+1
-                best_mse = ret[1]
-                best_ci = ret[-1]
-                print('rmse improved at epoch ', best_epoch, '; best_mse,best_ci:', best_mse,best_ci,model_st,dataset)
-            else:
-                print(ret[1],'No improvement since epoch ', best_epoch, '; best_mse,best_ci:', best_mse,best_ci,model_st,dataset)
+            ## training the model
+            device = torch.device(cuda_name if torch.cuda.is_available() else "cpu")
+            model = modeling().to(device)
+            loss_fn = nn.MSELoss()
+            optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+            
+            best_mse = 1000
+            best_ci = 0
+            best_epoch = -1
+            
+            model_file_name = f'model_{model_st}_{dataset}.model'
+            result_file_name = f'result_{model_st}_{dataset}.csv'
+            
+            for epoch in range(NUM_EPOCHS):
+                train(model, device, train_loader, optimizer, epoch+1)
+                
+                G, P = predicting(model, device, test_loader)
+                
+                ret = [rmse(G, P), mse(G, P), pearson(G, P), spearman(G, P), ci(G, P)]
+                
+                if ret[1] < best_mse:
+                    torch.save(model.state_dict(), model_file_name)
+                    with open(result_file_name, 'w') as f:
+                        f.write(','.join(map(str,ret)))
+                    best_epoch = epoch+1
+                    best_mse = ret[1]
+                    best_ci = ret[-1]
+                    print(f'rmse improved at epoch {best_epoch}; best_mse,best_ci: {best_mse} {best_ci} {model_st} {dataset}')
+                else:
+                    print(f'{ret[1]} No improvement since epoch {best_epoch}; best_mse,best_ci: {best_mse} {best_ci} {model_st} {dataset}')
 
+
+if __name__=='__main__':
+    
+    TRAIN_BATCH_SIZE = 512
+    TEST_BATCH_SIZE = 512
+    LR = 0.0005
+    LOG_INTERVAL = 20
+    NUM_EPOCHS = 1000
+    
+    main()
